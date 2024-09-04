d758f675cdf4600f8b3ca200b44e5cafd48e5fde
Conor McCloskey <conorgk@udel.edu>
2024-09-04 15:38:10 -0400

    Header, list w 3 elements, and background color of header

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,18 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
+            <h1>This is my header!</h1>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Conor McCloskey
             </p>
             <p> Hello World
             </p>
+            <ul>
+                <li>First item</li>
+                <li>Second item</li>
+                <li>Third Item</li>
+            </ul>
         </div>
     );
 }
